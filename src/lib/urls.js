14aea5e441da74b0f9cb21250bea2644e1330c48
WlodekM <75479945+WlodekM@users.noreply.github.com>
2023-05-04 10:40:24 +0300

    Update urls.js

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -6,6 +6,6 @@ export const linkUrl =
 	localStorage.getItem("meower_linkurl") ||
 	"wss://testcl.meower.org/";
 export const apiUrl =
-	localStorage.getItem("meower_apiurl") || "wss://testapi.meower.org/";
+	localStorage.getItem("meower_apiurl") || "https://testapi.meower.org/";
 export const encodeApiURLParams =
 	localStorage.getItem("meower_encodeapi") || false;
